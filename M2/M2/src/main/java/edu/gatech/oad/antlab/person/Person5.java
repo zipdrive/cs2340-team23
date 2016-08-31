b4b64bef74fc6d910cdfca131acc47cb95bf70d0
zipdrive <iamzachphillips@gmail.com>
2016-08-31 17:03:24 -0400

    Filled out the function in Person5.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Zachary Phillips
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,11 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String output = "";
+		for (int i = 0; i < input.length(); i++) {
+			output += input.charAt((i+2) % input.length());
+		}
+		return output;
 	}
 	
 	/**
