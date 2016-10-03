961be899a9632e3dfcbf452f7144d2c344fd2d28
zipdrive <iamzachphillips@gmail.com>
2016-10-02 21:28:19 -0400

    Fixed everything again
    Apprently the stuff I deleted in the last commit was important, so
    IntelliJ threw a hissy fit.

--- Project/Project/src/fxapp/MainApplication.java
+++ Project/Project/src/fxapp/MainApplication.java
@@ -4,7 +4,6 @@ import controller.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
